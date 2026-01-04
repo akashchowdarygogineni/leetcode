3b12d76db59f494b2786d16b777bd6e26c7027bd
Akash gogineni <160498524+akashchowdarygogineni@users.noreply.github.com>
2026-01-04 18:47:42 +0530

    Time: 9 ms (40.44%) | Memory: 42.1 MB (50.44%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -6,17 +6,6 @@ class Solution {
             return n;
         }
 
-        int secondlast=0;
-        int last=1;
-
-        int total=0;
-
-        for(int i=2;i<=n;i++)
-        {
-            total=last+secondlast;
-            secondlast=last;
-            last=total;
-        }
-        return total;
+       return fib(n-1)+fib(n-2);
     }
 }
