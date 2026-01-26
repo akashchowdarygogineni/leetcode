d8dd3957f2277cbee9917076c2c5c9858969e7d5
Akash gogineni <160498524+akashchowdarygogineni@users.noreply.github.com>
2026-01-26 18:55:21 +0530

    Time: 1 ms (26.47%) | Memory: 46.8 MB (38.08%) - LeetSync

---
+++ 898-transpose-matrix/transpose-matrix.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[][] transpose(int[][] mat) {
+        
+        int[][] newarr=new int[mat[0].length][mat.length];
+
+        for(int i=0;i<mat.length;i++)
+        {
+            for(int j=0;j<mat[0].length;j++)
+            {
+               newarr[j][i]=mat[i][j];
+            }
+        }
+
+        return newarr;
+    }
+}
