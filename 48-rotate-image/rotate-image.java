264a8be93ecddb648e8143f32690a198c23a37c1
Akash gogineni <160498524+akashchowdarygogineni@users.noreply.github.com>
2026-01-31 11:49:52 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (11.34%) - LeetSync

--- 48-rotate-image/rotate-image.java
+++ 48-rotate-image/rotate-image.java
@@ -1,21 +1,26 @@
 class Solution {
     public void rotate(int[][] matrix) {
-        
-        int n=matrix.length;
-        int[][] temp=new int[n][n];
+        //finding the transpose of the square matrix without using the extra variable
+        int n = matrix.length;
 
-        for(int i=0;i<matrix.length;i++)
-        {
-            for(int j=0;j<matrix[0].length;j++)
-            {
-                temp[j][matrix.length-i-1]=matrix[i][j];
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
             }
         }
 
-          // Copy back to original matrix
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                matrix[i][j] = temp[i][j];
+        //reverse the each row of the array
+
+
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<matrix.length/2;j++)
+            {
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[i][n-j-1];
+                matrix[i][n-j-1]=temp;
             }
         }
     }
