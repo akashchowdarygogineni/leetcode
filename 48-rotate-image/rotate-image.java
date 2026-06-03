bfb48bdaddec1c19d9d930351cf9fa0979bfffd6
Akash gogineni <160498524+akashchowdarygogineni@users.noreply.github.com>
2026-06-03 14:57:46 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (50.29%) - LeetSync

--- 48-rotate-image/rotate-image.java
+++ 48-rotate-image/rotate-image.java
@@ -3,7 +3,7 @@ class Solution {
         //finding the transpose of the square matrix without using the extra variable
         int n = matrix.length;
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n-1; i++) {
             for (int j = i + 1; j < n; j++) {
                 int temp = matrix[i][j];
                 matrix[i][j] = matrix[j][i];
