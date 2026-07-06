bc7531d8b53225f5466b9ffc533f441390178aa1
Akash gogineni <160498524+akashchowdarygogineni@users.noreply.github.com>
2026-07-06 16:42:43 +0530

    Time: 2 ms (70.08%) | Memory: 56 MB (6.49%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,65 +1,46 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        // int ele=Integer.MIN_VALUE;
-        // int count=0;
-        // for(int i=0;i<nums.length;i++)
-        // {
-        //    if(count==0)
-        //    {
-        //      ele=nums[i];
-        //      count++;
-        //    }
-        //    else if(nums[i]==ele)
-        //    {
-        //      count++;
+      
+             int ele=Integer.MIN_VALUE;
 
-        //    }
-        //    else{
-        //     count--;
-        //    }
-        // }
-         
-        // int count1=0;
-        // for(int i=0;i<nums.length;i++)
-        // {
-        //     if(nums[i]==ele)
-        //     {
-        //        count1++;
-        //     }
-           
-        // }
-
-        // if(count1>nums.length/2)
-        // {
-        //     return ele;
-        // }
-        // return -1;
-
-
+        int count=0;
 
         int n=nums.length;
-        HashMap<Integer,Integer> map=new HashMap<>();
-
         for(int i=0;i<nums.length;i++)
         {
-            if(!map.containsKey(nums[i]))
+            if(count==0)
             {
-                map.put(nums[i],1);
+                count++;
+                ele=nums[i];
+            }
+
+            else if(ele==nums[i])
+            {
+                count++;
             }
             else{
-                map.put(nums[i],map.get(nums[i])+1);
+                count--;
             }
+
         }
 
-        Set<Integer> s=map.keySet();
 
-        for(int i:s)
+
+        int count1=0;
+        for(int i:nums)
         {
-            if(map.get(i)>n/2)
+            if(i==ele)
             {
-                return i;
+                count1++;
             }
         }
+
+        if(count1>n/2)
+        {
+            return ele;
+
+        }
+
         return -1;
     }
 }
