78e391d6581fa2cf2c7fcfcb11cd97bb103a218e
Akash gogineni <160498524+akashchowdarygogineni@users.noreply.github.com>
2026-07-07 14:50:49 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (8.97%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.java
+++ 54-spiral-matrix/spiral-matrix.java
@@ -1,73 +1,48 @@
 class Solution {
-    public List<Integer> spiralOrder(int[][] matrix) {
+    public List<Integer> spiralOrder(int[][] arr) {
         ArrayList<Integer> li=new ArrayList<>();
-        
-        int n=matrix.length;
-        int m=matrix[0].length;
-        
-        int left=0;
-        int right=m-1;
         int top=0;
-        int bottom=n-1;
+        int bottom=arr.length-1;
 
-        if(n==1)
-        {
-            for(int j=0;j<m;j++)
-            {
-                li.add(matrix[0][j]);
-            }
-             return li;
-        }
-       
-        if(m==1)
-        {
-            for(int i=0;i<n;i++)
-            {
-                li.add(matrix[i][0]);
-            }
-            return li;
-        }
-        
-        
-  while(left<=right && top<=bottom)
-    {
+        int left=0;
+        int right=arr[0].length-1;
+
+       while(left<=right && top<=bottom)
+       {
         for(int i=left;i<=right;i++)
         {
-            li.add(matrix[top][i]);
+            li.add(arr[top][i]);
         }
+
         top++;
-        
+
         for(int i=top;i<=bottom;i++)
         {
-            li.add(matrix[i][right]);
+            li.add(arr[i][right]);
         }
         right--;
-        
-        if(top <= bottom){
-        
-             for(int i=right;i>=left;i--)
-             {
-              li.add(matrix[bottom][i]);
-            
-             }
-            bottom--;  }
-        
-     
-        
        
-        if(left<=right){
-           for(int i=bottom;i>=top;i--)
-            {
-             li.add(matrix
-             [i][left]);
-            }
-           left++;  
+       if(top<=bottom)
+       {
+        for(int i=right;i>=left;i--)
+        {
+            li.add(arr[bottom][i]);
         }
-        
-        
-        
- 
-    }
-    return li;
+        bottom--;
+       }
+
+
+       if(left<=right)
+       {
+        for(int i=bottom;i>=top;i--)
+        {
+           li.add(arr[i][left]);
+        }
+        left++;
+       }
+       }
+
+
+       return li;
     }
 }
