4252dcdf391542af080e12f44113b7d1f5e3f5d4
Akash gogineni <160498524+akashchowdarygogineni@users.noreply.github.com>
2026-07-13 12:25:21 +0530

    Time: 2 ms (94.89%) | Memory: 43.2 MB (24.32%) - LeetSync

---
+++ 4100-complete-prime-number/complete-prime-number.java
@@ -0,0 +1,47 @@
+class Solution {
+    public boolean completePrime(int num) {
+
+
+        String s=String.valueOf(num);
+
+
+        for(int i=1;i<=s.length();i++)
+            {
+                int prefix=Integer.parseInt(s.substring(0,i));
+
+                if(!isPrime(prefix))
+                {
+                    return false;
+                }
+            }
+
+        
+        for(int i=0;i<s.length();i++)
+            {
+                int suffix=Integer.parseInt(s.substring(i));
+
+                if(!isPrime(suffix))
+                {
+                    return false;
+                }
+            }
+
+        return true;
+        
+    }
+    public static boolean isPrime(int x)
+    {
+        if(x<2)
+        {
+            return false;
+        }
+        for(int i=2;i*i<=x;i++)
+            {
+                if(x%i==0)
+                {
+                    return false;
+                }
+            }
+        return true;
+    }
+}
