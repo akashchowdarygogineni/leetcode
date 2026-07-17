376a31507a4c1a01054dd6af1a16e58f2813c9c4
Akash gogineni <160498524+akashchowdarygogineni@users.noreply.github.com>
2026-07-17 17:06:19 +0530

    Time: 21 ms (13.22%) | Memory: 47.6 MB (15.66%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.java
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.java
@@ -1,9 +1,9 @@
 class Solution {
     public String frequencySort(String s) {
         
-        //create the hashmap and store the each character frequency
-
+        
         HashMap<Character,Integer> map=new HashMap<>();
+
 
         for(int i=0;i<s.length();i++)
         {
@@ -15,24 +15,25 @@ class Solution {
                 map.put(s.charAt(i),map.get(s.charAt(i))+1);
             }
         }
-        
-        //craete the list and place the all the charcteres in the lists
-        List<Character> li=new ArrayList<>(map.keySet());
-        
-        //sort the charcters by the frequency (deccesrsing order)
-        Collections.sort(li,(a,b)->map.get(b)-map.get(a));
 
-        StringBuilder sb=new StringBuilder();
 
-        for(char ch:li)
+    List<Map.Entry<Character,Integer>> li=new ArrayList<>(map.entrySet());
+
+
+    Collections.sort(li,(a,b)->(b.getValue()-a.getValue()));
+
+    StringBuilder sb=new StringBuilder();
+
+    for(Map.Entry<Character,Integer> tr:li)
+
+    {
+        for(int i=0;i<tr.getValue();i++)
         {
-            int k=map.get(ch);
-            for(int i=1;i<=k;i++)
-            {
-                sb.append(ch);
-            }
+            sb.append(tr.getKey());
         }
+      
+    }
 
-        return sb.toString();
+    return sb.toString();
     }
 }
