0b25e88e8d01b015b45bacbe08a8877e2df909c8
Akash gogineni <160498524+akashchowdarygogineni@users.noreply.github.com>
2026-07-19 22:32:27 +0530

    Time: 4 ms (19.25%) | Memory: 44 MB (18.26%) - LeetSync

--- 1987-substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.java
+++ 1987-substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.java
@@ -1,25 +1,64 @@
 class Solution {
     public int countGoodSubstrings(String s) {
-        
-
-        int count=0;
-        int k=3;
-
-        int n=s.length();
-
-        for(int i=0;i<=n-k;i++)
+         int k=3;
+       if (s.length() < k)
         {
-           HashSet<Character> set=new HashSet<>();
-           for(int j=i;j<i+k;j++)
-           {
-              set.add(s.charAt(j));
-           }
+        return 0;
+        }
+        int count=0;
+       
 
-           if(set.size()==k)
+        int left=0;
+        int right=k-1;
+
+         HashMap<Character,Integer> map=new HashMap<>();
+        for(int i=left;i<=right;i++)
+        {
+           if(!map.containsKey(s.charAt(i)))
            {
-             count++;
+            map.put(s.charAt(i),1);
+           }
+           else{
+            map.put(s.charAt(i),map.get(s.charAt(i))+1);
            }
         }
+
+        if(map.size()==k)
+        {
+            count++;
+        }
+
+
+        while(right<s.length()-1)
+        {
+            char ch=s.charAt(left);
+            map.put(ch,map.get(ch)-1);
+
+            if(map.get(ch)==0)
+            {
+                map.remove(ch);
+            }
+            left++;
+
+            right++;
+
+            char ch1=s.charAt(right);
+
+            if(!map.containsKey(ch1))
+            {
+                map.put(ch1,1);
+
+            }
+            else{
+                map.put(ch1,map.get(ch1)+1);
+            }
+
+            if(map.size()==k)
+            {
+                count++;
+            }
+        }
+
 
         return count;
     }
