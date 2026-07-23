c48974478e903622cb59ff0e4d9f0a6b5cb43fc5
Akash gogineni <160498524+akashchowdarygogineni@users.noreply.github.com>
2026-07-23 07:06:32 +0530

    Time: 0 ms (100.00%) | Memory: 48.4 MB (31.25%) - LeetSync

--- 792-binary-search/binary-search.java
+++ 792-binary-search/binary-search.java
@@ -5,22 +5,28 @@ class Solution {
         
     
         
-        while(low<=high)
+       return recursivebs(nums,low,high,target);
+    }
+
+    public static int recursivebs(int[] nums,int low,int high,int target)
+    {
+        //base condition
+        if(low>high)
         {
-            int mid=(low+high)/2;
-            if(nums[mid]==target)
-            {
-                return mid;
-            }
-            else if(nums[mid]<target)
-            {
-                low=mid+1;
-            }
-            else{
-                high=mid-1;
-            }
+            return -1;
         }
-        
-        return -1;
+
+        int mid=(low+high)/2;
+
+        if(nums[mid]==target)
+        {
+            return mid;
+        }
+
+        else if(nums[mid]<target)
+        {
+            return recursivebs(nums,mid+1,high,target);
+        }
+        return recursivebs(nums,low,mid-1,target);
     }
 }
