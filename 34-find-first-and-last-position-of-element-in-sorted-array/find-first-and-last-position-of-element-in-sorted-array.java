da2628f246c3a2d269de8f277be25915ced83cfe
Akash gogineni <160498524+akashchowdarygogineni@users.noreply.github.com>
2026-07-23 11:00:15 +0530

    Time: 0 ms (100.00%) | Memory: 48.3 MB (31.69%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,64 +1,73 @@
 class Solution {
 
-    public static int lowerBound(int[] nums, int target) {
+    public static int first(int[] nums, int target) {
         // code here
         int low=0;
         int high=nums.length-1;
         
     
-        int ans=nums.length;
+        int ans=-1;
         while(low<=high)
         {
             int mid=(low+high)/2;
-            if(nums[mid]>=target)
+            if(nums[mid]==target)
             {
                 ans=mid;
                 high=mid-1;
                 
             }
           
-            else{
+            else if(nums[mid]<target){
                 low=mid+1;
+            }
+            else{
+                high=mid-1;
             }
         }
         
         return ans;
     }
 
-    public static int upperBound(int[] nums, int target) {
+    public static int last(int[] nums, int target) {
         // code here
         int low=0;
         int high=nums.length-1;
         
     
-        int ans=nums.length;
+        int ans=-1;
         while(low<=high)
         {
             int mid=(low+high)/2;
-            if(nums[mid]>target)
+            if(nums[mid]==target)
             {
                 ans=mid;
-                high=mid-1;
+                low=mid+1;;
                 
+            }
+            else if(nums[mid]<target)
+            {
+               low=mid+1;
             }
           
             else{
-                low=mid+1;
+                high=mid-1;
             }
         }
         
         return ans;
     }
     public int[] searchRange(int[] nums, int target) {
-        
+        int f=first(nums,target);
 
-        int first=lowerBound(nums,target);
-        if(first==nums.length || nums[first]>target)
+        if(f==-1)
         {
             return new int[]{-1,-1};
         }
-        int last=upperBound(nums,target);
-        return new int[]{first,last-1};
+        int l=last(nums,target);
+
+        return new int[]{f,l};
+
+     
 
     }
 }
