c7c9abf76383be59cb7c2db5bded578afc1addec
Akash gogineni <160498524+akashchowdarygogineni@users.noreply.github.com>
2026-07-24 22:52:01 +0530

    Time: 1 ms (26.72%) | Memory: 43.7 MB (77.72%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -3,18 +3,68 @@ class Solution {
 
    
       
+      
+        int len=a.length;
+        int gap=(len/2)+(len%2);
+        
+        
+      
+      while(gap>0) {
+        
+        int left=0;
+        int right=left+gap;
+        
+        
+        while(right<len)
+        {
+            //there are three cases 
+            //if left is in a1 and right in a2
+            
+            if(left<m && right>=m)
+            {
+                swap(a,b,left,right-m);
+            }
+            
+            //case 2 both in a2
+            else if(left>=m)
+            {
+                swap(b,b,left-m,right-m);
+            }
+            
+            
+            //both in a1
+            else{
+                swap(a,a,left,right);
+            }
+            
+            
+            left++;
+            right++;
+        }
+        if(gap==1)
+        {
+            break;
+        }
+        gap=(gap/2)+ (gap % 2);
+    }
 
-      //
 
-      for(int i=0;i<b.length;i++)
-      {
+    for(int i=0;i<b.length;i++)
+    {
         a[m+i]=b[i];
-      }
-      Arrays.sort(a);
-     
+    }
+        
          
         
     }
-
+     public static void swap(int[] a,int[] b,int left,int right)
+    {
+        if(a[left]>b[right])
+        {
+            int temp=a[left];
+            a[left]=b[right];
+            b[right]=temp;
+        }
+    }
     
 }
