058ef157ffd250a86227b8665170e36555b10ef2
Akash gogineni <160498524+akashchowdarygogineni@users.noreply.github.com>
2026-07-24 22:23:34 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (16.02%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -1,62 +1,58 @@
 class Solution {
-    public void merge(int[] arr1, int m, int[] arr2, int n) {
+    public void merge(int[] a, int m, int[] b, int n) {
 
       
-       int len=(n+m);
-
-       int gap=(len/2)+(len%2);
-
-       while(gap>0)
-       {
-         int left=0;
-         int right=gap;
-
-         while(right<len)
-         {
-            //left is at arr1 and right is at arr2
-            if(left<m && right>=m)
+      int[] newarr=new int[m+n];
+        
+        int left=0;
+        int right=0;
+        
+        int index=0;
+        
+        while(left<m && right<n)
+        {
+            if(a[left]<=b[right])
             {
-               swapIfgreater(arr1,arr2,left,right-m);
+                newarr[index]=a[left];
+                left++;
+                index++;
             }
-
-            //left is at arr2 and right is at arr2
-            else if(left>=m && right>=m)
-            {
-                swapIfgreater(arr2,arr2,left-m,right-m);
-            }
-
-            //both on the left
             else{
-                swapIfgreater(arr1,arr1,left,right);
+                newarr[index]=b[right];
+                right++;
+                
+                index++;
             }
+        }
+        
+        while(left<m)
+        {
+            newarr[index]=a[left];
             left++;
+            index++;
+        }
+        
+        
+        while(right<n)
+        {
+            newarr[index]=b[right];
+        
             right++;
-         }
-         if(gap==1)
-         {
-            break;
-         }
-         gap=(gap/2)+(gap%2);
-       }
-
-       //place the all the elemnts of array2 in array1
-
-       for(int k=0;k<n;k++)
-       {
-         arr1[m++]=arr2[k];
-       }
+            index++;
+        }
+        
+        //then what you are doing you are palcing these elements in the orginal arrays
+        for(int i=0;i<newarr.length;i++)
+        {
+           
+                a[i]=newarr[i];
+            
+            
+            
+        }
          
         
     }
 
-     public static void swapIfgreater(int[] arr1,int[] arr2,int left,int right)
-    {
-        if(arr1[left]>arr2[right])
-        {
-            //swap
-            int temp=arr1[left];
-            arr1[left]=arr2[right];
-            arr2[right]=temp;
-        }
-    }
+    
 }
