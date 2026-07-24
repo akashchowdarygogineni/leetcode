82c5a4f1c578684e2862f20c4da840f65c48e116
Akash gogineni <160498524+akashchowdarygogineni@users.noreply.github.com>
2026-07-24 22:31:56 +0530

    Time: 3 ms (22.80%) | Memory: 43.9 MB (29.43%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -1,55 +1,37 @@
 class Solution {
     public void merge(int[] a, int m, int[] b, int n) {
 
+       int left=m-1;
       
-      int[] newarr=new int[m+n];
-        
-        int left=0;
-        int right=0;
-        
-        int index=0;
-        
-        while(left<m && right<n)
-        {
-            if(a[left]<=b[right])
-            {
-                newarr[index]=a[left];
-                left++;
-                index++;
-            }
-            else{
-                newarr[index]=b[right];
-                right++;
-                
-                index++;
-            }
-        }
-        
-        while(left<m)
-        {
-            newarr[index]=a[left];
-            left++;
-            index++;
-        }
-        
-        
-        while(right<n)
-        {
-            newarr[index]=b[right];
-        
-            right++;
-            index++;
-        }
-        
-        //then what you are doing you are palcing these elements in the orginal arrays
-        for(int i=0;i<newarr.length;i++)
-        {
-           
-                a[i]=newarr[i];
-            
-            
-            
-        }
+      int right=0;
+      
+      
+      while(left>=0 && right<b.length)
+      {
+          if(a[left]>b[right])
+          {
+              //swap those elements
+              int temp=a[left];
+              a[left]=b[right];
+              b[right]=temp;
+              left--;
+              right++;
+              
+          }
+          else{
+              break;
+          }
+      }
+      
+
+      //
+
+      for(int i=0;i<b.length;i++)
+      {
+        a[m+i]=b[i];
+      }
+      Arrays.sort(a);
+     
          
         
     }
