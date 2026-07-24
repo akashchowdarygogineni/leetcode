ee9374652af27ba5c6f1d08b3011ac5abbc9dfe8
Akash gogineni <160498524+akashchowdarygogineni@users.noreply.github.com>
2026-07-24 22:32:17 +0530

    Time: 4 ms (17.61%) | Memory: 43.9 MB (29.43%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -1,27 +1,7 @@
 class Solution {
     public void merge(int[] a, int m, int[] b, int n) {
 
-       int left=m-1;
-      
-      int right=0;
-      
-      
-      while(left>=0 && right<b.length)
-      {
-          if(a[left]>b[right])
-          {
-              //swap those elements
-              int temp=a[left];
-              a[left]=b[right];
-              b[right]=temp;
-              left--;
-              right++;
-              
-          }
-          else{
-              break;
-          }
-      }
+   
       
 
       //
